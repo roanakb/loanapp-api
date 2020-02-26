beefd8cd5a38262600f1c8a96f0aeb56610767dc
Roanak Baviskar <roanakbaviskar@gmail.com>
2020-02-26 13:46:40 -0800

    testing file add

--- test.py
+++ test.py
@@ -7,14 +7,15 @@ with open('sample.json') as json_file:
     #print(f'{business_name} and {business_data} ')
     all_owner_data = data['Owners']
     #print(all_owner_data)
-    for entry in business_data:
-        value = business_data[entry]
-        if not value:
-            pass
-        elif type(value) == type(data):
-            for e in value:
-                inner_val = value[e]
-                if inner_val:
-                    existing_data[entry][e] = inner_val
-        else:
-            existing_data[entry] = value
+    for owner_dict in all_owner_data:
+        for entry in owner_dict:
+            value = owner_dict[entry]
+            if not value:
+                pass
+            elif type(value) == type(data):
+                for e in value:
+                    inner_val = value[e]
+                    if inner_val:
+                        existing_owner_data[entry][e] = inner_val
+            else:
+                existing_owner_data[entry] = value
